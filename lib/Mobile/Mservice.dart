8be88951e22d4b3fa76a8b66ad75e7235782bb34
Isuru <isurumbandara@gmail.com>
2024-03-09 22:41:17 +0530

    small changes

--- lib/Mobile/Mservice.dart
+++ lib/Mobile/Mservice.dart
@@ -125,8 +125,7 @@ class _MserviceState extends State<Mservice> with TickerProviderStateMixin {
                               height: 15,
                             ),
                             Padding(
-                              padding:
-                                  const EdgeInsets.only(left: 10, right: 10),
+                              padding: EdgeInsets.only(left: 10, right: 10),
                               child: Text(
                                 "I specialize in crafting responsive web applications using flutter. Delivering user-friendly websites with seamless navigation. Ensuring optimal performance and scalability. Additionally, I implement robust backend solutions to enhance functionality.",
                                 textAlign: TextAlign.center,
@@ -167,7 +166,7 @@ class _MserviceState extends State<Mservice> with TickerProviderStateMixin {
                                   ),
                                 ],
                               ),
-                              SizedBox(height: 2),
+                              const SizedBox(height: 2),
                               Row(
                                 children: [
                                   Image.asset(
