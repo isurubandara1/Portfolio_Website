da3056ebc5b0eaf7a0f38ed8a256d05ee43cb9fc
Isuru <isurumbandara@gmail.com>
2024-03-13 01:37:59 +0530

    change home  page

--- lib/DeskTop/Home.dart
+++ lib/DeskTop/Home.dart
@@ -86,190 +86,194 @@ class _AnimatedContentState extends State<AnimatedContent>
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Text and buttons
-          SlideTransition(
-            position: _textOffsetAnimation,
-            child: FadeTransition(
-              opacity: _fadeInTextAnimation,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "👋🏻 𝗛𝗶, 𝗧𝗵𝗲𝗿𝗲!",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  RichText(
-                    text: const TextSpan(
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25, right: 25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text and buttons
+            SlideTransition(
+              position: _textOffsetAnimation,
+              child: FadeTransition(
+                opacity: _fadeInTextAnimation,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "👋🏻 𝗛𝗶, 𝗧𝗵𝗲𝗿𝗲!",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    RichText(
+                      text: const TextSpan(
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                        children: [
+                          TextSpan(
+                            text: "I'm ",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 45,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Isuru Bandara", // Change name here
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 45,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Mobile App Developer with a passion for creating seamless digital experiences.",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w100,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const Text(
+                      "Excited about the future of mobile tech, let's connect and bring your app ideas to life!",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w100,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Image.asset(
+                              "assets/linkdin.png",
+                              fit: BoxFit.fill,
+                              width: 27,
+                            ),
+                            onPressed: () {
+                              _launchURL(
+                                  'https://www.linkedin.com/in/isuru-bandara-b51aab244/');
+                            },
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          IconButton(
+                            icon: Image.asset(
+                              "assets/github.png",
+                              fit: BoxFit.fill,
+                              width: 40,
+                            ),
+                            onPressed: () {
+                              _launchURL('https://github.com/isurubandara1');
+                            },
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          IconButton(
+                            icon: Image.asset(
+                              "assets/stackflow.png",
+                              fit: BoxFit.fill,
+                              width: 38,
+                            ),
+                            onPressed: () {
+                              _launchURL(
+                                  'https://stackoverflow.com/users/16578521/isuru-bandara');
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
                       children: [
-                        TextSpan(
-                          text: "I'm ",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 45,
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Contact()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7.0),
+                              side: const BorderSide(
+                                  color: Colors.blue, width: 2.0),
+                            ),
+                            minimumSize: Size(120, 60),
                           ),
+                          child: const Text("Contact"),
                         ),
-                        TextSpan(
-                          text: "Isuru Bandara", // Change name here
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 45,
+                        const SizedBox(width: 16),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black12,
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7.0),
+                              side: const BorderSide(
+                                  color: Colors.blue, width: 2.0),
+                            ),
+                            minimumSize: Size(120, 60),
                           ),
+                          child: const Text("Download CV"),
                         ),
                       ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    "Mobile App Developer with a passion for creating seamless digital experiences.",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w100,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const Text(
-                    "Excited about the future of mobile tech, let's connect and bring your app ideas to life!",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w100,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          icon: Image.asset(
-                            "assets/linkdin.png",
-                            fit: BoxFit.fill,
-                            width: 27,
-                          ),
-                          onPressed: () {
-                            _launchURL(
-                                'https://www.linkedin.com/in/isuru-bandara-b51aab244/');
-                          },
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        IconButton(
-                          icon: Image.asset(
-                            "assets/github.png",
-                            fit: BoxFit.fill,
-                            width: 40,
-                          ),
-                          onPressed: () {
-                            _launchURL('https://github.com/isurubandara1');
-                          },
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        IconButton(
-                          icon: Image.asset(
-                            "assets/stackflow.png",
-                            fit: BoxFit.fill,
-                            width: 38,
-                          ),
-                          onPressed: () {
-                            _launchURL(
-                                'https://stackoverflow.com/users/16578521/isuru-bandara');
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Contact()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.blue,
-                          onPrimary: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(7.0),
-                            side: const BorderSide(
-                                color: Colors.blue, width: 2.0),
-                          ),
-                          minimumSize: Size(120, 60),
-                        ),
-                        child: const Text("Contact"),
-                      ),
-                      const SizedBox(width: 16),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black12,
-                          onPrimary: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(7.0),
-                            side: const BorderSide(
-                                color: Colors.blue, width: 2.0),
-                          ),
-                          minimumSize: Size(120, 60),
-                        ),
-                        child: const Text("Download CV"),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
 
-          // Spacer
-          const SizedBox(width: 16),
+            // Spacer
+            const SizedBox(width: 16),
 
-          // Profile image
-          SlideTransition(
-            position: _imageOffsetAnimation,
-            child: Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                // border: Border.all(
-                //    color: Colors.blue,
-                //   width: 2,
-                // ),
-              ),
-              child: ClipOval(
-                child: SizedBox(
-                  width: 450,
-                  height: 450,
-                  child: Image.asset(
-                    "assets/1.png",
-                    fit: BoxFit.fill,
+            // Profile image
+            SlideTransition(
+              position: _imageOffsetAnimation,
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  // border: Border.all(
+                  //    color: Colors.blue,
+                  //   width: 2,
+                  // ),
+                ),
+                child: ClipOval(
+                  child: SizedBox(
+                    width: 450,
+                    height: 450,
+                    child: Image.asset(
+                      "assets/1.png",
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
