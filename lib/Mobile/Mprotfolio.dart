5f38a72101046ed16fa968fabe044a3b9d23bf3a
Isuru <isurumbandara@gmail.com>
2024-03-19 14:54:49 +0530

    add github projects

--- lib/Mobile/Mprotfolio.dart
+++ lib/Mobile/Mprotfolio.dart
@@ -350,7 +350,10 @@ class Mprotfolio extends StatelessWidget {
                           height: 10,
                         ),
                         ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              _launchURL(
+                                  'https://github.com/isurubandara1/Random_Users');
+                            },
                             style: ElevatedButton.styleFrom(
                               primary: Colors.black12,
                               onPrimary: Colors.white,
