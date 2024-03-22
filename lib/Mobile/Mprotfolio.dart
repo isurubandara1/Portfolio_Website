e29ed7507218826f40ba7d9054dc974ba717b952
Isuru <isurumbandara@gmail.com>
2024-03-22 22:08:14 +0530

    add github projects

--- lib/Mobile/Mprotfolio.dart
+++ lib/Mobile/Mprotfolio.dart
@@ -221,7 +221,10 @@ class Mprotfolio extends StatelessWidget {
                           height: 10,
                         ),
                         ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              _launchURL(
+                                  'https://github.com/isurubandara1/Endemic_Animals_App');
+                            },
                             style: ElevatedButton.styleFrom(
                               primary: Colors.black12,
                               onPrimary: Colors.white,
